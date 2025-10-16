e8c9ddb34a46db1a668546c31a4225f981b1dc16
Manuel Elias Rodriguez Romero <manu@manurdgz.com>
2025-10-16 14:51:18 -0600

    2025-10-16

--- Q3.R
+++ Q3.R
@@ -17,3 +17,19 @@ data <- read_excel("DATAGUIA1B.xlsx")
 data <- data.frame(data)
 am <- data$AM
 spy <- data$SPY
+
+am_ret <- diff(log(am))
+spy_ret <- diff(log(spy))
+
+am_model <- lm(am_ret ~ spy_ret)
+beta0_am <- coef(am_model)[1]
+beta1_am <- coef(am_model)[2]
+
+plot(spy_ret, am_ret, main = "Retornos AM vs SPY", xlab = "SPY", ylab = "AM", col = "darkgreen", pch = 19)
+abline(am_model, col = "red")
+
+am_ret_mean <- mean(am_ret)
+am_ret_var <- var(am_ret)
+am_ret_sd <- sd(am_ret)
+cov_matrix_am <- cov(cbind(am_ret, spy_ret))
+cor_matrix_am <- cor(cbind(am_ret, spy_ret))
