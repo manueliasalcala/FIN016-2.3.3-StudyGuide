7589683289a2e0c5c95a6c2992b74e5dff60e35b
Manuel Elias Rodriguez Romero <manu@manurdgz.com>
2025-10-15 18:41:13 -0600

    2025-10-15

---
+++ Q2.R
@@ -0,0 +1,36 @@
+library(urca)
+library(tseries)
+library(astsa)
+library(forecast)
+library(foreign)
+library(timsac)
+library(vars)
+library(mFilter)
+library(dynlm)
+library(nlme)
+library(COINT)
+library(dplyr)
+library(dplyrAssist)
+library(quantmod)
+library(readxl)
+data <- read_excel("DATAGUIA1B.xlsx")
+data <- data.frame(data)
+f1 <- data$F1
+spy <- data$SPY
+
+f1_ret <- diff(log(f1))
+spy_ret <- diff(log(spy))
+
+f1_model <- lm(f1_ret ~ spy_ret)
+beta0_f1 <- coef(f1_model)[1]
+beta1_f1 <- coef(f1_model)[2]
+
+plot(spy_ret, f1_ret, xlab = "F1", ylab = "NVDIA")
+abline(f1_model, col="red", lwd=2)
+
+f1_ret_mean <- mean(f1_ret)
+f1_ret_var <- var(f1_ret)
+f1_ret_sd <- sd(f1_ret)
+f1_ret_cov_m <- cov(cbind(f1_ret, spy_ret))
+f1_ret_cor_m <- cor(cbind(f1_ret, spy_ret))
+f1_cv <- f1_ret_sd / f1_ret_mean
