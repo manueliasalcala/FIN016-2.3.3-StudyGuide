667868b2978360e6aeb17359dd4292fc5cda4d47
Manuel Elias Rodriguez Romero <manu@manurdgz.com>
2025-10-15 18:22:09 -0600

    2025-10-15

--- Q1.R
+++ Q1.R
@@ -20,16 +20,21 @@ nvdia <- data$NVIDIA
 #Get summary statistics.
 nvdia_mean <- mean(nvdia)
 nvdia_var <- var(nvdia)
+nvdia_sd <- sd(nvdia)
 #Calculate returns.
 nvdia_ret <- diff(log(nvdia))
+#Returns stats
+nvdia_ret_mean <- mean(nvdia_ret)
+nvdia_ret_sd <- sd(nvdia_ret)
 #Graph returns.
 hist(nvdia_ret)
-ts.plot(nvdia_ret, main = "Rendimientos NVDIA", xlab = "Días", ylab = "Precios")
+ts.plot(nvdia_ret, main = "Rendimientos NVDIA", xlab = "Días", ylab = "Precios", col = "magenta")
 #Get nabla
 nvdia_nabla <- diff(nvdia)
 ts.plot(nvdia_nabla, main = "Diferencial de NVDIA", xlab = "Días",
         ylab = "Precios")
-nvdia_cor<- sum(head(nvdia - mean(nvdia), -1) * tail(nvdia - mean(nvdia), -1)) / sum((nvdia - mean(nvdia))^2)
+nvdia_cor1 <- sum(head(nvdia - mean(nvdia), -1) * tail(nvdia - mean(nvdia), -1)) / sum((nvdia - mean(nvdia))^2)
+nvdia_cor2 <- sum(head(nvdia - mean(nvdia), -2) * tail(nvdia - mean(nvdia), -2)) / sum((nvdia - mean(nvdia))^2)
 acf(nvdia, lag.max = 3)
 acf(nvdia, main = "Correlograma NVDIA")
 
